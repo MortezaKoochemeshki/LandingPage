b079603d8c83d18528db8963f56ddf740d2707ec
MortezaKoochemeshki <117094234+MortezaKoochemeshki@users.noreply.github.com>
2023-05-09 11:31:42 +0330

    Add files via upload

---
+++ LandingPages/JavaScript/LandingPage.js
@@ -0,0 +1,8 @@
+function imageSlider(imageSource){
+    document.querySelector('.pepsi').src = imageSource;
+}
+
+function changeBgColor(color){
+    const section = document.querySelector('.sec');
+    section.style.background = color;
+}
